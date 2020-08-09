690efbf919aab884e943636c906831c34aa19b41
zhangguicong08679 <zhangguicong08679@hellobike.com>
2020-08-09 16:52:46 +0800

    netty

--- netty/src/main/java/netty/example/codec/NettyClient.java
+++ netty/src/main/java/netty/example/codec/NettyClient.java
@@ -1,18 +1,40 @@
 package netty.example.codec;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
 
 /**
  * @author zhangguicong
  * @date 2020-08-09
  */
 public class NettyClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         EventLoopGroup group = new NioEventLoopGroup();
 
         try {
+            // 客户端启动对象
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast("encoder",new ProtobufEncoder());
+                            pipeline.addLast(new NettyClientHandler());
+                        }
+                    });
+            System.out.println("client ready...");
 
+            // 启动客户端去连接服务器端
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 18888).sync();
+            channelFuture.channel().closeFuture().sync();
         } finally {
             group.shutdownGracefully();
         }
